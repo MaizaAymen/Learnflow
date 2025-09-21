785b3cc5f29593e5c1e6776aad9de19d7ab37765
MaizaAymen <aymenmaiza87@gmail.com>
2025-09-21 23:19:05 +0100

    complete the registre and the login

--- backend/auth-service/routes/authRoutes.js
+++ backend/auth-service/routes/authRoutes.js
@@ -23,6 +23,28 @@ catch (error) {
 
 }})
 
+router.post("/login", async (req, res) => {
+    try {
+      const { login, mdp } = req.body;
+      const user = await utilisateur.findOne({where: { login }});
+      if (!user) {
+        return res.status(404).json({ error: "Utilisateur non trouvé" });
+      }
+
+      const isPasswordValid = await bcrypt.compare(mdp, user.mdp_hash);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Mot de passe incorrect" });
+      }
+
+      const token = jwt.sign({ id: user.id, role: user.role }, secretKey, {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de la connexion de l'utilisateur" });
+    }
+  });
+
 module.exports = router;
 
 
