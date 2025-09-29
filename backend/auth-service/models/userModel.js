7b7fc43279b8ebf98ce5751bf72531b413a96800
MaizaAymen <aymenmaiza87@gmail.com>
2025-09-29 12:24:48 +0100

    i add cin on the student model

--- backend/auth-service/models/userModel.js
+++ backend/auth-service/models/userModel.js
@@ -14,7 +14,8 @@ const User =sequelize.define('User',{
   role: { type: DataTypes.ENUM('etudiant','enseignant','directeur','admin'), allowNull: false },
   image: { type: DataTypes.STRING, allowNull: true }, // url ou chemin
   phone: { type: DataTypes.STRING, allowNull: true },
-  bio: { type: DataTypes.TEXT, allowNull: true }
+  bio: { type: DataTypes.TEXT, allowNull: true },
+  cin:{type: DataTypes.STRING, allowNull: true}
 }, {
   schema: "auth",
   tableName: "utilisateur"
